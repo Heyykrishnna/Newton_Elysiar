85c463b867e8caae2240c12e5e6b153da8f3ceea
Yatharth <khandelwalyatharth39@gmail.com>
2025-12-26 01:35:48 +0530

    newfea

--- src/components/BadgeDetailModal.tsx
+++ src/components/BadgeDetailModal.tsx
@@ -177,7 +177,7 @@ export function BadgeDetailModal({
               onClick={onClose}
               variant="outline"
               className={cn(
-                "border-white/20 text-white hover:bg-white/10",
+                "border-white/20 black hover:bg-white/10 hover:text-white",
                 !isUnlocked && "flex-1"
               )}
             >
